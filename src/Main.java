2416cb7f115ae71ebc8092dc7549741bea3bb5d7
Artem Timoshenko <resp.kll@ya.ru>
2023-09-19 21:20:52 +0300

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,35 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
-    private static String fileList = "";
 
     public static void main(String[] args) {
         GameProgress gProgress1 = new GameProgress(100, 200, 10, 3.4);
         GameProgress gProgress2 = new GameProgress(50, 0, 2, 0.5);
         GameProgress gProgress3 = new GameProgress(74, 112, 4, 2.8);
-        List<String> namesList = Arrays.asList("C://Games", "C://Games/savegames",
-                "C://Games/savegames/save1.dat", "C://Games/savegames/save2.dat",
-                "C://Games/savegames/save3.dat");
+        List<String> namesList = Arrays.asList("D://Games", "D://Games/savegames",
+                "D://Games/savegames/save1.dat", "D://Games/savegames/save2.dat",
+                "D://Games/savegames/save3.dat");
         for (int i = 0; i < namesList.size(); i++) {
             String name = namesList.get(i);
             Create(name);
         }
 
-        saveGame("C://Games/savegames/save1.dat", gProgress1);
-        saveGame("C://Games/savegames/save2.dat", gProgress2);
-        saveGame("C://Games/savegames/save3.dat", gProgress3);
-        getListFiles("C://Games/savegames", "C://Games/savegames/fileList.txt");
+        saveGame("D://Games/savegames/save1.dat", gProgress1);
+        saveGame("D://Games/savegames/save2.dat", gProgress2);
+        saveGame("D://Games/savegames/save3.dat", gProgress3);
+        zipFiles("D://Games/savegames/zip.zip", "D://Games/savegames");
 
     }
 
-    public static void Create(String nameFile) {
+    private static void Create(String nameFile) {
         int index = nameFile.indexOf(".");
         if (index == -1) {
             File dir = new File(nameFile);
@@ -40,7 +44,7 @@ public class Main {
         }
     }
 
-    public static void saveGame(String filePath, GameProgress gameProgress) {
+    private static void saveGame(String filePath, GameProgress gameProgress) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
             oos.writeObject(gameProgress);
         } catch (IOException ex) {
@@ -48,20 +52,27 @@ public class Main {
         }
     }
 
-    public static void getListFiles(String dirPath, String filePath) {
-        File dir = new File(dirPath);
-        if (dir.isDirectory()) {
-            for (File item : dir.listFiles()) fileList += dirPath + "/" + item.getName() + "\n";
-        }
-        try (FileOutputStream fos = new FileOutputStream(filePath)) {
-            byte[] bytes = fileList.getBytes();
-            fos.write(bytes, 0, bytes.length);
-        } catch (IOException ex) {
+    private static void zipFiles(String filePath, String folderPath) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(filePath))) {
+            File folder = new File(folderPath);
+            for (File file : folder.listFiles()) {
+                if (file.getName().contains(".dat")) {
+                    FileInputStream fis = new FileInputStream(file.getAbsoluteFile());
+                    ZipEntry entry = new ZipEntry(file.getName());
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                    fis.close();
+                    file.delete();
+                }
+            }
+
+        } catch (
+                Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
-
-    public static void zipFiles(String filePath, String fileList) {
-
-    }
 }
+
